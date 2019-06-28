bcbccb32e7eeab6c4157a7d299f091a88ae18565
YangLiufeng <zhaoruihuang@163.com>
2019-06-28 09:22:53 +0800

    增加3,4

--- src/main/java/com/itheima/main.java
+++ src/main/java/com/itheima/main.java
@@ -3,5 +3,7 @@ package com.itheima;
 public class main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
     }
 }
